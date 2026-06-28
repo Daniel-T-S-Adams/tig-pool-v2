56efce1878556279291730dd61d08ed737c3c86e
rootztigmod <rootz.tig.mod@gmail.com>
2026-06-28 18:17:39 +0100

    Force pool manager info logging

--- pool_manager/main.py
+++ pool_manager/main.py
@@ -18,6 +18,7 @@ from pool import tracker, coinbase, scheduler, autopilot, ai_optimizer
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+    force=True,
 )
 logger = logging.getLogger("pool_manager")
 
